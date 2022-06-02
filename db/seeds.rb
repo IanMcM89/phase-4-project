4d899d0b6e25fc7c793480abb061105bbfd7c7d6
Ian <ianmcm.89@gmail.com>
2022-06-02 03:59:38 -0400

    added lorem ipsum comment data

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,7 +66,7 @@ Workout.create(
 
 User.second.reviews.create(
   id: 1,
-  comment: "",
+  comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   rating: 3,
   user_id: 2,
   workout_id: 1
@@ -74,7 +74,7 @@ User.second.reviews.create(
 
 Review.create(
   id: 2,
-  comment: "",
+  comment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   rating: 3,
   user_id: 2,
   workout_id: 2
@@ -82,7 +82,7 @@ Review.create(
 
 Review.create(
   id: 3,
-  comment: "",
+  comment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   rating: 3,
   user_id: 2,
   workout_id: 3
