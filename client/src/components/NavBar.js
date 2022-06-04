06c661f36211e219af5015b0a69663e7413a894f
Ian <ianmcm.89@gmail.com>
2022-06-04 04:18:31 -0400

    added Logo component to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Header } from "../styles";
+import { Header, Logo, Button } from "../styles";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function NavBar({ setUser }) {
+function NavBar({ user, setUser }) {
   function handleLogout() {
     fetch("/api/logout", {
       method: "DELETE"
@@ -15,19 +16,52 @@ function NavBar({ setUser }) {
 
   return (
     <Header>
-      <nav>
-        <Link to="/">
-          <button>Home</button>
+      <Logo style={{justifyContent: "left"}}>
+        <img
+          src="/images/logo_2.png"
+          style={{
+            display: "flex",
+            width: "60px",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+          alt="App Logo" />
+        <Link style={{textDecoration: "none"}} to="/">
+          <Title>Fitness App</Title>
         </Link>
-        <Link to="/create">
-          <button>Create Workout</button>
-        </Link>
-        <button onClick={handleLogout}>
+      </Logo>
+      <Nav>
+        <Message>Welcome {user.username}!</Message>
+        <Button variant="orange" onClick={handleLogout}>
           Logout
-        </button>
-      </nav>
+        </Button>
+      </Nav>
     </Header>
   )
 }
+
+const Title = styled.h1`
+  font-family: "Permanent Marker", cursive;
+  font-size: 2rem;
+  color: white;
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  width: 49%;
+  height: 100%;
+  margin: 0 1% 0 auto;
+`;
+
+const Message = styled.p`
+  font-family: cursive;
+  color: white;
+  font-size: 1rem;
+  text-align: center;
+  margin: auto 2% auto 2%;
+`;
 
 export default NavBar
--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -5,5 +5,6 @@ import Form from "./Form";
 import FormField from "./FormField";
 import Input from "./Input";
 import Label from "./Label";
+import Logo from "./Logo";
 
-export { Header, Button, Error, Form, FormField, Input, Label };
+export { Header, Button, Error, Form, FormField, Input, Label, Logo };
