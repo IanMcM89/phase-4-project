c3c2f6a30a9572ad9c0b533c1aab04815ea910ce
Ian <ianmcm.89@gmail.com>
2022-06-04 04:19:21 -0400

    adjusted header styling

--- client/src/styles/Header.js
+++ client/src/styles/Header.js
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const Header = styled.header`
   background-color: darkblue;
-  border-bottom: 1rem solid orangered;
+  border-bottom: 0.75rem solid orangered;
   display: flex;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
   width: auto;
   height: 10vh;
+  margin: 0;
   top: 0;
 `;
 
