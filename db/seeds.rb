b23bf8239e95d8222516ee40f374ff1009de37b9
Ian <ianmcm.89@gmail.com>
2022-06-08 09:01:09 -0400

    refactor comment sees and add new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,13 @@ User.create(
   password_confirmation: "password1"
 )
 
+User.create(
+  id: 3,
+  username: "User_02",
+  password: "password2",
+  password_confirmation: "password2"
+)
+
 Muscle.create(
   id: 1, 
   group: "chest",
@@ -69,7 +76,7 @@ Workout.create(
   id: 1,
   title: "Pushups", 
   image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
-  description: "", 
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam sit amet nisl suscipit. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Auctor elit sed vulputate mi sit amet mauris commodo. In pellentesque massa placerat duis ultricies lacus. Risus quis varius quam quisque id diam vel quam. Accumsan in nisl nisi scelerisque eu ultrices. Venenatis lectus magna fringilla urna.", 
   sets: 3,
   reps: 12, 
   is_weighted: false, 
@@ -82,7 +89,7 @@ Workout.create(
   id: 2,
   title: "Crunches", 
   image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
-  description: "", 
+  description: "Ultricies mi quis hendrerit dolor magna eget est lorem ipsum. Scelerisque in dictum non consectetur a erat nam at. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. A arcu cursus vitae congue mauris rhoncus.", 
   sets: 3,
   reps: 12, 
   is_weighted: false, 
@@ -95,7 +102,7 @@ Workout.create(
   id: 3,
   title: "Pull Ups", 
   image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
-  description: "", 
+  description: "Odio morbi quis commodo odio aenean. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Tortor condimentum lacinia quis vel eros. Viverra nam libero justo laoreet sit amet cursus sit amet.", 
   sets: 3,
   reps: 12, 
   is_weighted: true, 
@@ -108,7 +115,7 @@ Workout.create(
   id: 4,
   title: "Tricep Dips", 
   image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
-  description: "", 
+  description: "Massa tincidunt dui ut ornare lectus sit amet est. Leo a diam sollicitudin tempor id eu. Lectus proin nibh nisl condimentum id venenatis a condimentum vitae.", 
   sets: 3,
   reps: 12, 
   is_weighted: false, 
@@ -121,7 +128,7 @@ Workout.create(
   id: 5,
   title: "Weighted Squats", 
   image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
-  description: "", 
+  description: "Faucibus a pellentesque sit amet porttitor eget dolor morbi. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Velit scelerisque in dictum non consectetur a. Mattis nunc sed blandit libero volutpat. Risus viverra adipiscing at in tellus integer feugiat scelerisque.", 
   sets: 3,
   reps: 12, 
   is_weighted: true, 
@@ -130,50 +137,93 @@ Workout.create(
   posted_by: "user1"
 )
 
-3.times do 
-  Comment.create(
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    user_id: 2,
-    workout_id: 1
-  )
-end
+Workout.create(
+  id: 6,
+  title: "Incline Bench", 
+  image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
+  description: "Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Sem fringilla ut morbi tincidunt. Nulla pharetra diam sit amet nisl suscipit adipiscing. Amet cursus sit amet dictum sit amet justo donec. Sagittis nisl rhoncus mattis rhoncus urna neque viverra.", 
+  sets: 3,
+  reps: 12, 
+  is_weighted: true, 
+  muscle_id: 1,
+  target_muscles: ["Pecs"],
+  posted_by: "User_02"
+)
 
-3.times do 
+Workout.create(
+  id: 7,
+  title: "Plank", 
+  image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
+  description: "Quisque non tellus orci ac auctor. Tempus egestas sed sed risus pretium quam vulputate dignissim. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et.", 
+  sets: 3,
+  reps: 1, 
+  is_weighted: false, 
+  muscle_id: 2,
+  target_muscles: ["Abdominals"],
+  posted_by: "user1"
+)
+
+Workout.create(
+  id: 8,
+  title: "Deadlifts", 
+  image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
+  description: "Purus gravida quis blandit turpis cursus. Aliquet risus feugiat in ante. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et.", 
+  sets: 3,
+  reps: 8, 
+  is_weighted: true, 
+  muscle_id: 3,
+  target_muscles: ["Erector Spinae"],
+  posted_by: "User_02"
+)
+
+Workout.create(
+  id: 9,
+  title: "Skull Crushers", 
+  image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
+  description: "Imperdiet sed euismod nisi porta lorem mollis aliquam ut. Faucibus pulvinar elementum integer enim neque volutpat ac. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Eget dolor morbi non arcu. ", 
+  sets: 3,
+  reps: 8, 
+  is_weighted: true, 
+  muscle_id: 4,
+  target_muscles: ["Triceps"],
+  posted_by: "ianmcm89"
+)
+
+Workout.create(
+  id: 10,
+  title: "Pistol Squats", 
+  image_url: "https://cdn.pixabay.com/photo/2014/05/18/18/28/icon-347233_960_720.png", 
+  description: "Augue ut lectus arcu bibendum. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. In iaculis nunc sed augue lacus viverra vitae.", 
+  sets: 3,
+  reps: 12, 
+  is_weighted: true, 
+  muscle_id: 5,
+  target_muscles: ["Glutes", "Quads", "Hamstrings", "Calves"],
+  posted_by: "User_02"
+)
+
+# Comments Seeds
+
+Workout.all.each  do |w| 
   Comment.create(
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     user_id: 1,
-    workout_id: 2
+    workout_id: w.id
   )
 end
 
-3.times do
+Workout.all.each  do |w| 
   Comment.create(
-    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    message: "Augue ut lectus arcu bibendum.",
     user_id: 2,
-    workout_id: 2
+    workout_id: w.id
   )
 end
 
-3.times do
+Workout.all.each  do |w| 
   Comment.create(
-    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    user_id: 1,
-    workout_id: 3
-  )
-end
-
-3.times do
-  Comment.create(
-    message: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    user_id: 2,
-    workout_id: 3
-  )
-end
-
-3.times do
-  Comment.create(
-    message: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    user_id: 1,
-    workout_id: 1
+    message: "Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies.",
+    user_id: 3,
+    workout_id: w.id
   )
 end
