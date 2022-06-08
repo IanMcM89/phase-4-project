d04b9265fac8542b9b812c7b4243421746dcdfff
Ian <ianmcm.89@gmail.com>
2022-06-07 21:47:44 -0400

    removed :updated_at

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :message, :user_id, :workout_id, :created_at, :updated_at 
+  attributes :id, :message, :user_id, :workout_id, :created_at
 
   belongs_to :user, serializer: CommentUserSerializer
   belongs_to :workout, serializer: MuscleWorkoutSerializer
