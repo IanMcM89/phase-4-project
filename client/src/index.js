b26c4b2398c604dba473585d3b761391faf32c57
Ian <ianmcm.89@gmail.com>
2022-06-08 08:54:45 -0400

    change background to lightgrey

--- client/src/index.js
+++ client/src/index.js
@@ -18,8 +18,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: url("/images/background.png") repeat;
-    background-size: 400px;
+    background-color: lightgray;
     font-family: BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
     overflow: hidden;
   }
