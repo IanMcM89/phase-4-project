1956c030353d1495640281bbd531a911e279f66d
Ian <ianmcm.89@gmail.com>
2022-06-08 09:00:30 -0400

    create Textarea component

---
+++ client/src/styles/Textarea.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  display: flex;
+  background-color: gainsboro;
+  border-radius: 6px;
+  border: 1px solid gray;
+  font-size: 16px;
+  width: 100%;
+  height: 90%;
+  margin: 0;
+`;
+
+export default Textarea;
