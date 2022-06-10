761ab633ea14000ee92840f751824a2b45a919b5
Ian <ianmcm.89@gmail.com>
2022-06-10 05:35:59 -0400

    slow down animation speed

--- client/src/styles/Form.js
+++ client/src/styles/Form.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Form = styled.form`
   width: 70%;
-  animation: appear 1.4s ease forwards;
+  animation: appear 1.8s ease forwards;
 `;
 
 export default Form;
