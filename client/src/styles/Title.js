18757c4ce652c1acd7b8d5c249a18f199842157e
Ian <ianmcm.89@gmail.com>
2022-06-10 05:34:17 -0400

    create Title.js

---
+++ client/src/styles/Title.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: royalblue;
+  font-family: "Permanent Marker", cursive;
+  font-size: 2rem;
+  margin: 0;
+`;
+
+export default Title;
