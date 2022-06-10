cabce8a5140e4253f62aea55b536f17eb1a04b52
Ian <ianmcm.89@gmail.com>
2022-06-10 05:35:00 -0400

    remove justify-content and margin properties

--- client/src/styles/Logo.js
+++ client/src/styles/Logo.js
@@ -3,10 +3,8 @@ import styled from "styled-components";
 const Logo = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
   align-items: center;
   height: 20%;
-  margin: 8px 0 16px;
   animation: hoverOut 0.4s ease forwards;
 
   :hover {
