1172e6ddda9992e01c7dad1d21398e590fdd52b4
Ian <ianmcm.89@gmail.com>
2022-06-10 21:48:11 -0400

    add static and dynamic muscle group headers

--- client/src/components/BodyMap.js
+++ client/src/components/BodyMap.js
@@ -5,9 +5,11 @@ function BodyMap({ filterWorkouts }) {
   const [body, setBody] = useState(null);
   const [source, setSource] = useState("/images/body/default.png");
   const [muscles, setMuscles] = useState([]);
+  const [spanStatic, setSpanStatic] = useState(null);
+  const [spanDynamic, setSpanDynamic] = useState(null);
 
   useEffect(() => {
-    setBody(document.getElementById("default"))
+    setBody(document.getElementById("default"));
   }, []);
 
   useEffect(() => {
@@ -16,7 +18,8 @@ function BodyMap({ filterWorkouts }) {
       .then(setMuscles)
   }, []);
 
-  const handleMouseOver = (e) => {
+  const handleMouseOver = (e, muscle) => {
+    setSpanDynamic(muscle);
     const src = `/images/body/${e.target.id}.png`;
     switch (e.target.id) {
       case "chest":
@@ -40,16 +43,41 @@ function BodyMap({ filterWorkouts }) {
   };
 
   const handleMouseOut = () => {
+    setSpanDynamic(null);
     body.src = source;
   };
 
-  const handleClick = (e) => {
+  const handleClick = (e, muscle) => {
+    setSpanStatic(muscle ? muscle : null);
+
     setSource(`/images/body/${e.target.id}.png`);
     filterWorkouts(e.target.id);
   }
 
+  const renderSpan = () => {
+    let pText;
+    
+    if (spanDynamic) {
+      pText = spanDynamic
+    } else if (spanStatic) {
+      pText = spanStatic
+    } else {
+      pText = null
+    }
+
+    return (
+      <>
+        <P1>{pText ? pText.title : null}</P1>
+        <P2>{pText ? pText.sub_groups.join(", ") : null}</P2>
+      </>
+    )
+  }
+
   return (
     <Wrapper>
+      <Span>
+        {renderSpan()}
+      </Span>
       <Figure>
         <Img
           id="default"
@@ -72,8 +100,8 @@ function BodyMap({ filterWorkouts }) {
               alt={muscle.title}
               shape="poly"
               coords={muscle.coords}
-              onClick={handleClick}
-              onMouseOver={handleMouseOver}
+              onClick={(e) => handleClick(e, muscle)}
+              onMouseOver={(e) => handleMouseOver(e, muscle)}
               onMouseOut={handleMouseOut}
             />
           )}
@@ -86,6 +114,7 @@ function BodyMap({ filterWorkouts }) {
 const Wrapper = styled.div`
   background-color: white;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 60%;
@@ -95,16 +124,37 @@ const Wrapper = styled.div`
 `;
 
 const Figure = styled.figure`
+  display: flex;
   margin: 0;
   animation: appear 1.8s ease forwards;
 `;
 
+const Span = styled.span`
+  position: absolute;
+  top: 12%;
+  left: 2%;
+  margin: 0;
+  z-index: 1;
+`;
+
+const P1 = styled.p`
+  color: darkblue;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin: 0;
+`;
+
+const P2 = styled(P1)`
+  color: goldenrod;
+  font-size: 1rem
+`;
+
 const Img = styled.img`
   border: 0, 
-  width: "640px", 
-  height: "640px", 
-  orgWidth: "640px", 
-  orgHeight: "640px"
+  width: 600px; 
+  height: 600px;
+  orgWidth: 600px;
+  orgHeight: 600px;
 `;
 
 export default BodyMap;
