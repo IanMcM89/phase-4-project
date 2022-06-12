266f7f1c58120d89d9dc5339c559babbfc7e2b2c
Ian <ianmcm.89@gmail.com>
2022-06-12 06:01:10 -0400

    wrap form and add background

--- client/src/pages/NewExercise.js
+++ client/src/pages/NewExercise.js
@@ -52,118 +52,120 @@ function NewExercise({ user }) {
 
   return (
     <Wrapper>
-      <PostLogo>
-        <img src="/images/logo_1.png" style={{ width: "100px" }} alt="App Logo" />
-        <Title variant="post">Create Exercise</Title>
-      </PostLogo>
-      <PostForm onSubmit={handleSubmit}>
-        <Section1>
-          <FormField>
-            <Label htmlFor="title">Title</Label>
-            <Input
-              type="text"
-              id="title"
-              placeholder="Push Ups..."
-              value={formData.title}
-              onChange={handleChange}
-            />
-          </FormField>
-          <FormField>
-            <Label htmlFor="image_url">Image URL:</Label>
-            <Input
-              type="url"
-              id="image_url"
-              placeholder="https://..."
-              value={formData.image_url}
-              onChange={handleChange}
-            />
-          </FormField>
-          <FormField style={{ height: "100%" }}>
-            <Label htmlFor="description">Method</Label>
-            <Textarea
-              type="textarea"
-              id="description"
-              placeholder="Maximum 500 characters..."
-              value={formData.description}
-              onChange={handleChange}
-              style={{ height: "80%" }}
-            />
-          </FormField>
-        </Section1>
-        <Section2>
-          <FormField>
-            <Label htmlFor="muscle_id">Muscle Group</Label>
-            <Select id="muscle_id" onChange={handleChange}>
-              {muscles.map(muscle => (
-                <option key={muscle.id} value={muscle.id}>
-                  {muscle.title}
-                </option>
-              ))}
-            </Select>
-          </FormField>
-          <FormField>
-            <Label htmlFor="is_weighted">Weights Required ?</Label>
-            <Input
-              type="checkbox"
-              id="is_weighted"
-              value={formData.is_weighted}
-              onChange={(e) => {
-                setFormData(
-                  { ...formData, [e.target.id]: !formData.is_weighted }
-                );
-              }}
-              style={{ width: "auto" }}
-            />
-          </FormField>
-          <FormField>
-            <RangeLabel htmlFor="sets">
-              Sets
-              {
-                <Output id="amount" htmlFor="sets">
-                  {formData.sets}
-                </Output>
-              }
-            </RangeLabel>
-            <Input
-              type="range"
-              id="sets"
-              min="1"
-              max="10"
-              value={formData.sets}
-              onChange={handleChange}
-            />
-          </FormField>
-          <FormField>
-            <RangeLabel htmlFor="reps">
-              Reps
-              {
-                <Output id="amount" htmlFor="reps">
-                  {formData.reps}
-                </Output>
-              }
-            </RangeLabel>
-            <Input
-              type="range"
-              id="reps"
-              min="1"
-              max="100"
-              value={formData.reps}
-              onChange={handleChange}
-            />
-          </FormField>
-          <FormField>
-            <Button type="submit">
-              {loading ? "Loading..." : "Submit"}
-            </Button>
-          </FormField>
-          <FormField>
-            {errors.map((error) =>
-              <Error key={error}>{error}</Error>
-            )}
-          </FormField>
-        </Section2>
-      </PostForm>
-      <Overlay variant="up" />
+      <WrapperForm>
+        <PostLogo>
+          <img src="/images/logo_1.png" style={{ width: "100px" }} alt="App Logo" />
+          <Title variant="post">Create Exercise</Title>
+        </PostLogo>
+        <PostForm onSubmit={handleSubmit}>
+          <Section1>
+            <FormField>
+              <Label htmlFor="title">Title</Label>
+              <Input
+                type="text"
+                id="title"
+                placeholder="Push Ups..."
+                value={formData.title}
+                onChange={handleChange}
+              />
+            </FormField>
+            <FormField>
+              <Label htmlFor="image_url">Image URL:</Label>
+              <Input
+                type="url"
+                id="image_url"
+                placeholder="https://..."
+                value={formData.image_url}
+                onChange={handleChange}
+              />
+            </FormField>
+            <FormField style={{ height: "100%" }}>
+              <Label htmlFor="description">Method</Label>
+              <Textarea
+                type="textarea"
+                id="description"
+                placeholder="Maximum 500 characters..."
+                value={formData.description}
+                onChange={handleChange}
+                style={{ height: "80%" }}
+              />
+            </FormField>
+          </Section1>
+          <Section2>
+            <FormField>
+              <Label htmlFor="muscle_id">Muscle Group</Label>
+              <Select id="muscle_id" onChange={handleChange}>
+                {muscles.map(muscle => (
+                  <option key={muscle.id} value={muscle.id}>
+                    {muscle.title}
+                  </option>
+                ))}
+              </Select>
+            </FormField>
+            <FormField>
+              <Label htmlFor="is_weighted">Weights Required ?</Label>
+              <Input
+                type="checkbox"
+                id="is_weighted"
+                value={formData.is_weighted}
+                onChange={(e) => {
+                  setFormData(
+                    { ...formData, [e.target.id]: !formData.is_weighted }
+                  );
+                }}
+                style={{ width: "auto" }}
+              />
+            </FormField>
+            <FormField>
+              <RangeLabel htmlFor="sets">
+                Sets
+                {
+                  <Output id="amount" htmlFor="sets">
+                    {formData.sets}
+                  </Output>
+                }
+              </RangeLabel>
+              <Input
+                type="range"
+                id="sets"
+                min="1"
+                max="10"
+                value={formData.sets}
+                onChange={handleChange}
+              />
+            </FormField>
+            <FormField>
+              <RangeLabel htmlFor="reps">
+                Reps
+                {
+                  <Output id="amount" htmlFor="reps">
+                    {formData.reps}
+                  </Output>
+                }
+              </RangeLabel>
+              <Input
+                type="range"
+                id="reps"
+                min="1"
+                max="100"
+                value={formData.reps}
+                onChange={handleChange}
+              />
+            </FormField>
+            <FormField>
+              <Button type="submit">
+                {loading ? "Loading..." : "Submit"}
+              </Button>
+            </FormField>
+            <FormField>
+              {errors.map((error) =>
+                <Error key={error}>{error}</Error>
+              )}
+            </FormField>
+          </Section2>
+        </PostForm>
+        <Overlay variant="up" />
+      </WrapperForm>
     </Wrapper>
   )
 }
@@ -173,6 +175,15 @@ const commonStyles = css`
 `;
 
 const Wrapper = styled.div`
+  ${commonStyles}
+  background: url("/images/background.png") repeat;
+  background-size: 400px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+`;
+
+const WrapperForm = styled.div`
   ${commonStyles}
   background-color: white;
   flex-direction: column;
